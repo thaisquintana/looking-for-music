a36ffec96f0a1875c0a4e2f13efb475296eb3d66
Thais Quitana <thaisquintanap@gmail.com>
2019-06-16 16:55:32 -0300

    serve

--- serve.js
+++ serve.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const serveStatic = require ('serve-static')
+const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, 'dist')))
 
-const port = process.env.PORT || 8080
-app.listen(port)
-
-console.log('Listening on port:', + port)
+app.listen(process.env.PORT || 8080, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  )
+})
